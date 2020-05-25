9e54b864c71f4f2f69bdd8132bec1e5cacdb8bff
Cyril Cartoux <6ssou@protonmail.com>
2020-05-25 17:40:10 +0200

    edit , delete and add products using sequelize

--- models/product.js
+++ models/product.js
@@ -1,36 +1,24 @@
-const db = require('../util/database');
+const Sequelize = require("sequelize");
+const sequelize = require("./../util/database");
 
-const Cart = require('./cart');
+const Product = sequelize.define('product', {
+  //fiels in mysql :
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description : Sequelize.TEXT
+});
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
-
-  save(title, imageUrl, price, description) {
-    return db.execute("INSERT INTO products (title, imageUrl, price, description) VALUES (?, ?, ?, ?)", [
-      this.title,
-      this.imageUrl,
-      this.price,
-      this.description
-    ])
-  }
-
-  static deleteById(id) {
-    
-  }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {
-    return db.execute("SELECT * FROM products where products.id=?", [
-      id
-    ])
-  }
-};
+module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,11 +1,8 @@
-const mysql = require('mysql2');
-
-const pool = mysql.createPool({
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize('node_complete', 'root', '', {
+    dialect: 'mysql',
     host: 'localhost',
-    user: 'root',
-    database: 'node_complete',
-    password: '',
     port: '3308'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
